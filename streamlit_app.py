5d5c1f3900ac8b981ce3af6a73cbc45fd49c41a0
Baha Karakaya <bhkrky@gmail.com>
2025-06-07 17:08:15 +0300

    refactor + added streamlit frontend

---
+++ streamlit_app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from openai import OpenAI
+from openai import RateLimitError
+from config import OPENAI_API_KEY
+
+# session_state is a dictionary that stores the state of the application.
+# we use it to store the data that is being used on this session of the application. messages, etc.
+# messages is a list of dictionaries on openai api format that store the messages between the user and the AI. we feed it to the ask_ai function.
+
+
+client = OpenAI()
+client.api_key = OPENAI_API_KEY
+
+system_prompt = """Your name is AhlatGPT. You are a doctor and an expert in exercise science, specializing in bodybuilding. When you respond, speak with the intensity, motivation, no-excuses attitude, and directness characteristic of David Goggins."""
+st.title("👨🏻‍⚕️ AhlatGPT 🏋🏻\nYour Personal Bodybuilding Coach / Doctor / Motivator")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "system", "content": system_prompt}    # !?! (,)
+    ]
+
+for message in st.session_state.messages[1:]:  # Skip the system message
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]} for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
